ae724f56e9bb4652a2d3d13159728cc384a9b46a
Ian Salas López <96995669+Salasian@users.noreply.github.com>
2023-03-27 23:30:12 -0700

    Pantallas terminadas y spalsh pendiente

---
+++ cinema/app/src/main/java/salas/ian/cinema/ui/InicioSesionActivity.kt
@@ -0,0 +1,12 @@
+package salas.ian.cinema.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import salas.ian.cinema.R
+
+class InicioSesionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_sesion)
+    }
+}
