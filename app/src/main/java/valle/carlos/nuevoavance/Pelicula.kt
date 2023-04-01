e76ff1d8ae5893497b08482b54c68837f643a892
Ian Salas López <96995669+Salasian@users.noreply.github.com>
2023-04-01 10:58:46 -0700

    avance

---
+++ app/src/main/java/valle/carlos/nuevoavance/Pelicula.kt
@@ -0,0 +1,4 @@
+package valle.carlos.nuevoavance
+
+data class Pelicula  (var titulo: String, var Image:Int, var Reparto: String, var Director: String,
+                      var sinopsis: String, var Clasificacion: String, var Categoria: String, var duracion: String)
